883f97c5e1d95585a677693849b6c5ec648d5dec
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-06-28 16:49:34 -0400

    created components for banner & cart

---
+++ src/components/Cart.js
@@ -0,0 +1,17 @@
+function Cart () {
+  const monsteraPrice = 8;
+  const ivyPrice = 10;
+  const flowerPrice = 15;
+  return (<div>
+    <h3>Cart</h3>
+    <ul>
+      <li>Monstera: { monsteraPrice }€</li>
+      <li>Ivy: { ivyPrice }€</li>
+      <li>Flower: { flowerPrice }€</li>
+    </ul>
+    <p>Total: { monsteraPrice + ivyPrice + flowerPrice }€</p>
+  </div>
+  );
+}
+
+export default Cart;
