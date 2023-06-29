0ed1a2ea05c5f9b3bc5ba5d0ea8f94fd84b26a59
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-06-29 10:51:22 -0400

    styled banner & cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,10 @@
+import '../styles/Cart.css'
+
 function Cart () {
   const monsteraPrice = 8;
   const ivyPrice = 10;
   const flowerPrice = 15;
-  return (<div>
+  return (<div className='lmj-cart'>
     <h3>Cart</h3>
     <ul>
       <li>Monstera: { monsteraPrice }€</li>
