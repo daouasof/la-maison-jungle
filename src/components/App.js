f8692647305e0320942dad148a7c5ad68a28f8b4
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-07-04 14:23:33 -0400

    added button to empty the cart

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,18 @@ import Cart from './Cart';
 import ShoppingList from './ShoppingList';
 import logo from '../assets/logo.png';
 import Footer from './Footer';
+import '../styles/Layout.css'
 
 function App() {
   return <div>
-    	<Banner>
+      <Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-
-			<Cart />
-			<ShoppingList />
+			<div className='lmj-layout-inner'>
+				<Cart />
+				<ShoppingList />
+			</div>
 			<Footer />
     </div>
 }
