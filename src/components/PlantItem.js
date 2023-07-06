e8a8b7e6d1e8fb6a1f70189ba4849b6535df8f47
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-07-06 11:09:03 -0400

    added style on the list

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,12 +1,17 @@
 import CareScale from "./CareScale"
 import '../styles/PlantItem.css'
 
-function PlantItem({ name, cover, light, water}) {
+function PlantItem({ name, cover, light, water, price}) {
   return <li className='lmj-plant-item' >
+    <span className='lmj-plant-item-price'>{price}€</span>
     <img src={cover} alt={ name } className='lmj-plant-item-cover' />
-    { name }
-    <CareScale scaleValue={light} careType='light'/>
-    <CareScale scaleValue={water} careType='water'/>
+    <div className="lmj-plant-item-description">
+      { name }
+      <div className="lmj-plant-carescale">
+        <CareScale scaleValue={water} careType='water'/>
+        <CareScale scaleValue={light} careType='light'/>
+      </div>
+    </div>
   </li>
 }
 
